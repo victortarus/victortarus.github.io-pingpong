45a21c3c4fb525ba7b699704af94f2b50ac97d0a
victor tarus <victorkibet25@gmail.com>
2017-05-05 17:03:34 +0300

    added readme

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# The ping pong game.
+
+## Specifications
+1. This is a web application that takes a number from a user and returns a range of numbers from 1 to the chosen number with the following exceptions:
+
+..1  Numbers divisible by 3 are replaced with "ping"
+..2  Numbers divisible by 5 are replaced with "pong"
+..3  Numbers divisible by 15 are replaced with "pingpong"
+..4  A user should be able to enter a new number and see new results over and over again.
+
+### Project by:
+   Victor tarus[Github profile](https://github.com/victortarus)
+##### Copyright 
+[License:CC-BY](https://creativecommons.org/licenses/by/3.0)
---
+++ js/script.js
@@ -0,0 +1,35 @@
+//Business Logic
+var countUp = function(countTo){
+  var result = [];
+  for(var i = 1; i <= countTo; i++){
+      var index = result.indexOf(i);
+      if(i % 15=== 0) {
+        result.splice(index, 0);
+        result.push('pingpong');
+      }else if(i % 5 === 0) {
+        result.splice(index, 0);
+        result.push('pong');
+      }else if(i % 3 === 0) {
+        result.splice(index, 0);
+        result.push('Ping');
+      }else{
+        result.push(i);
+      }
+  }
+
+  return result;
+};
+
+//User Interface Logic
+$(document).ready(function(){
+  $("#userInput").submit(function(event){
+    $("#output").empty();
+    var countTo = parseInt($("input#countTo").val());
+    var output = countUp(countTo);
+
+    output.forEach(function(item){
+      $("#output").append('<li>' + item + '</li>');
+    });
+    event.preventDefault();
+  });
+});
